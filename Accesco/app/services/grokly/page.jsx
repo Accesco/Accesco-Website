f6fdd671d19885f7820c17e792a92bf6b3343328
rishsv <rishwanthsv17@gmail.com>
2026-06-30 09:36:07 +0530

    fix: resolve page.jsx merge conflict to fix lint error

--- Accesco/app/services/grokly/page.jsx
+++ Accesco/app/services/grokly/page.jsx
@@ -54,11 +54,7 @@ const dishes = {
   },
   butter: {
     name: 'Paneer Butter Masala',
-<<<<<<< HEAD
     image: 'https://images.unsplash.com/photo-1708782341807-ed35fc16b4ea?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-=======
-    image: 'https://images.unsplash.com/photo-1603894584373-5ac82b2ae398?w=600&auto=format&fit=crop&q=80',
->>>>>>> origin/main
     itemsCount: 5,
     price: 237,
     ingredients: [
